7a960dd2e8796c637cfef3ce1befeb9135b23a43
tlouro-c <tomascorreia.lc@gmail.com>
2024-05-16 12:12:56 +0100

    Fixed: timezone

--- backend/usermanagement/usermanagement/usermanagement/settings.py
+++ backend/usermanagement/usermanagement/usermanagement/settings.py
@@ -145,7 +145,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-	"ACCESS_TOKEN_LIFETIME": timedelta(minutes=3),
+	"ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
 
